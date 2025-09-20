e8a1def2ab85fa34c033fe851687791e4a18d45f
Nadul_Laknidu <nadullaknidu7@gmail.com>
2025-09-20 16:05:17 +0530

    Delete assets directory

--- assets/php/extract.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include 'assets/php/config.php';
-    $all_articles = $pdo->query('SELECT * FROM news ORDER BY publish_date DESC')->fetchAll();
-    $news_categories = [ ];
-    foreach ( $all_articles as $article ) { $news_categories [ $article [ 'category' ] ] [ ] = $article;}
-?>
